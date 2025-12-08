42c83406f11e7bc26a0b56d0c5c819f2134cccce
RishithSuresh <rishithsuresh10@gmail.com>
2025-12-08 21:58:22 +0530

    Small Change

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     sepolia: {
-      url: "https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID",
-      accounts: ["YOUR_TESTNET_PRIVATE_KEY"]
+      url: "https://sepolia.infura.io/v3/517956bbe21b46928f30138a28ab9373",
+      accounts: ["fsQYJ2AMI+Gr88AbUP41zMMqGviXAbsU0vJ+S5pMX7LEowHS8bREgw"]
     }
   }
 };
